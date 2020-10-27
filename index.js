8f3b52bfba7b9bcd0c593b03b358037888051086
Aakash Goel <aakash.lpin@gmail.com>
2020-10-27 18:38:29 +0530

    refocus on conflicting session

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ import axios from 'axios';
 import express from 'express';
 
 const { default: PQueue } = require('p-queue');
-const queue = new PQueue({ concurrency: 2 });
 
+const { PORT, MESSAGE_HANDLER_URI } = process.env;
+
+const queue = new PQueue({ concurrency: 2 });
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 4004;
-const MESSAGE_HANDLER_URI = process.env.MESSAGE_HANDLER_URI || 'http://localhost:3000/api/integrations/whatsapp/subscriber';
-
 async function fire(data) {
-  await axios.post(MESSAGE_HANDLER_URI, data);
+  try {
+    await axios.post(MESSAGE_HANDLER_URI, data);
+  } catch (e) {
+    console.log('fire error');
+    console.log(e);
+  }
 }
 
 const wh = (event) => async (data) => {
@@ -40,7 +44,11 @@ async function start(client) {
   client.onContactAdded(wh('contact_added'));
   client.onIncomingCall(wh('incoming_call'));
   client.onPlugged(wh('plugged'));
-  client.onStateChanged(wh('state'));
+  client.onStateChanged((state) => {
+    console.log('stateChanged', state);
+    if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.forceRefocus();
+    if (state === 'UNPAIRED') console.log('LOGGED OUT!!!!');
+  });
 
   // this is only for insiders
   client.onRemovedFromGroup(wh('removed_from_group'));
@@ -48,9 +56,15 @@ async function start(client) {
   const unreadMessages = await client.getAllUnreadMessages();
   unreadMessages.forEach(wh('message'));
 
+  client.getPage().on('error', (e) => {
+    console.log('client page error', e);
+  });
+
   app.listen(PORT, () => {
     console.log(`\n• Whatsapp Web listening on port ${PORT}!`);
-    console.log(`\n• Whatsapp Web is using URI ${MESSAGE_HANDLER_URI} for message handling`);
+    console.log(
+      `\n• Whatsapp Web is using URI ${MESSAGE_HANDLER_URI} for message handling`,
+    );
   });
 }
 
@@ -58,12 +72,14 @@ create({
   headless: true,
   autoRefresh: true,
   cacheEnabled: false,
+  inDocker: true,
+  // kill the process if the browser crashes/is closed manually
+  killProcessOnBrowserClose: true,
   // executablePath:
   //   '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-  executablePath:
-    'google-chrome-stable',
+  executablePath: 'google-chrome-stable',
 })
   .then(async (client) => await start(client))
   .catch((e) => {
-    console.log('Error', e.message);
+    console.log('Globally caught error', e.message);
   });
